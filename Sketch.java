cca675f6aa3c31838b923c4f4dbffdb66b356e72
Jacky Wang <jacky.wang23@ycdsbk12.ca>
2022-03-30 22:35:24 -0400

    Complete

--- Sketch.java
+++ Sketch.java
@@ -16,21 +16,56 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	
+   // quadrant one
+   
+   for (int lineX = 20; lineX <= 199; lineX += 20){
+    strokeWeight(2);
+    line(lineX, 0, lineX, 200);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
+   }
+     
+   for (int lineY = 20; lineY <= 200; lineY += 20){
+     line(0, lineY, 200, lineY);
+    }
+
+ // quadrant two
+   for (int circleX = 230; circleX <= 390; circleX += 35){
+     for (int circleY = 30; circleY <= 190; circleY += 35){
+       strokeWeight(1);
+       fill(255, 20, 147);
+       ellipse(circleX, circleY, 23, 23);
+       }
+     }
+
+ // quadrant three
+   for (int line = 0; line <= (width/2); line+=1){
+     for (int i = 0; i<width/2; i++){
+       stroke(i, i, i);
+       line(i, 0+height/2, i, height);
+        } 
+     }
+
+ // quadrant four
+   int petal = 0;
+
+   translate(height / 1.3f, width / 1.3f);
+   for (petal = 0; petal < 8; petal += 1) {
+     rotate(TWO_PI / 8);
+     fill(0, 100, 0);
+    ellipse(0, 0, height / 12, width / 12);
+     fill(252, 111, 3);
+     stroke(1);
+     ellipse(0, 0, height / 3, width / 24);
+     fill(0, 100, 0);
+     ellipse(0, 0, height / 12, width / 12);
+   } 
+ }
 }
